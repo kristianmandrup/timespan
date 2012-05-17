9388a9e10e930416677cf528f4e2111a300c1675
Kristian Mandrup <kmandrup@gmail.com>
2012-05-17 15:30:51 +0200

    better mongoid specs

--- spec/timespan/mongoid/mongoid_timespan_spec.rb
+++ spec/timespan/mongoid/mongoid_timespan_spec.rb
@@ -8,12 +8,24 @@ describe Timespan do
 
   context '2 days duration (from now - default)' do
     let(:account) do 
-      Account.create :period => {:duration => '2 days', :from => Date.today }
+      Account.create :period => {:duration => '2 days'}
     end
 
     describe '.start_date' do
       it 'should default to today' do
         DateTime.parse(subject.period.start_date.to_s).strftime('%d %b %Y').should == Date.today.strftime('%d %b %Y')
+      end
+    end
+  end
+
+  context '2 days duration (from 1 day ago)' do
+    let(:account) do 
+      Account.create :period => {:duration => '2 days', :from => from }
+    end
+
+    describe '.start_date' do
+      it 'should default to today' do
+        DateTime.parse(subject.period.start_date.to_s).strftime('%d %b %Y').should == 1.day.ago.strftime('%d %b %Y') 
       end
     end
   end
